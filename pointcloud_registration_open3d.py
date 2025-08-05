30893f362ba7fa83432bc4824daacd67700203b3
flying_matrix <135624430+FlyingMatrix@users.noreply.github.com>
2025-08-05 16:10:50 +0200

    Add files via upload

---
+++ pointcloud_registration_open3d.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import open3d as o3d
+import copy
+import time
+
+
+# Visualize registration result
+def draw_registration_result(source, target, transformation):
+
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4459,
+                                      front=[0.9288, -0.2951, -0.2242],
+                                      lookat=[1.6784, 2.0612, 1.4451],
+                                      up=[-0.3402, -0.9189, -0.1996])
+
+
+# Extract geometric feature
+def preprocess_point_cloud(pcd, voxel_size):
+
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+
+# Load point cloud and prepare dataset
+def prepare_dataset(voxel_size):
+
+    print(":: Load two point clouds and disturb initial pose.")
+    demo_icp_pcds = o3d.data.DemoICPPointClouds()
+    source = o3d.io.read_point_cloud(demo_icp_pcds.paths[0])
+    target = o3d.io.read_point_cloud(demo_icp_pcds.paths[1])
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+
+# Global registration with RANSAC
+def execute_global_registration(source_down, target_down, source_fpfh, target_fpfh, voxel_size):
+
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+
+    result_ransac = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down,
+        source_fpfh, target_fpfh,
+        mutual_filter=True,
+        max_correspondence_distance=0.075,
+        estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        ransac_n=3,
+        checkers=[
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)
+        ],
+        criteria=o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
+    
+    return result_ransac
+
+
+# Fine alignment using ICP
+def refine_registration(source, target, result_ransac, voxel_size):
+
+    distance_threshold = voxel_size * 0.04
+    print(":: Point-to-plane ICP registration is applied on original point")
+    print("   clouds to refine the alignment. This time we use a strict")
+    print("   distance threshold %.3f." % distance_threshold)
+
+    result_icp = o3d.pipelines.registration.registration_icp(
+        source, target,
+        max_correspondence_distance=0.02,
+        init=result_ransac.transformation,
+        estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPlane(),
+        criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=50))
+    
+    return result_icp
+
+
+if __name__ == "__main__":
+
+    # Prepare dataset
+    voxel_size = 0.01 
+    source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(voxel_size)
+
+    # Run global registration
+    start = time.time()
+    result_ransac = execute_global_registration(source_down, target_down,
+                                                source_fpfh, target_fpfh,
+                                                voxel_size)
+    print("::Global registration took %.3f sec.\n" % (time.time() - start))
+
+    # Fine alignment
+    start = time.time()
+    result_icp = refine_registration(source, target, 
+                                     result_ransac,
+                                     voxel_size)
+    print("::Fine alignment took %.3f sec.\n" % (time.time() - start))
+
+    # Print transformation matrix
+    print("::Transformation matrix:\n" + str(result_icp.transformation))
+
+    # Visualization
+    draw_registration_result(source, target, result_icp.transformation)
+
+
